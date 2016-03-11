d3b713e19f2ebc66acdb9bbbb4b1d87ffebb6719
wikibady <hysuperliu@foxmail.com>
2016-03-11 10:52:56 +0800

    1

--- main.js
+++ main.js
@@ -1,133 +1,119 @@
 
-var listHeight=[];
-var column=0;
+			var listHeight=[];//存储每行高度
+			var column=0;//计算每行列数
 
-function WhenToBottom(){//瀑布流1号
-	$(":text")[0].value=Math.round($(window).scrollTop());
-	$(":text")[1].value=$(document).height();
-	$(":text")[2].value=$(window).height();
-	$(":text")[3].value=$(document).height() - $(window).height();
-	if (Math.round($(window).scrollTop()) > Math.round($(document).height()) - Math.round($(window).height())-5) {
-		var list =$(".main");
-		//alert("A");
-		$.ajax({url:"./load.html",data:"html",success:function(result){
-			SetMainHeight();
-			list.append(result);
-			boxlist=StartAtLeft(".box1");
-			for(var i=0;i<boxlist.length;i++)
-			{
-				boxlist[i].className="box";
-			}
-			var j=0;
-			for(var i =0;i<boxlist.length;i++)
-				{
-					j=getMin(listHeight);
-					
-					if(j==0)//第一行
+			function WhenToBottom(){//滚动监听，到底部加载
+				if (Math.round($(window).scrollTop()) > Math.round($(document).height()) - Math.round($(window).height())-5) {
+					var list =$(".main");
+					$.ajax({url:"./load.html",data:"html",success:function(result){
+						SetMainHeight();//调用成功更新页面高度
+						list.append(result);//载入数据
+						boxlist=StartAtLeft(".box1");//初始化位置
+						for(var i=0;i<boxlist.length;i++)
 						{
-							
-							var left = ($(".main").width()-column*200-50)/2+'px';
+							boxlist[i].className="box";
 						}
-					else 
-					{
-						  var left =($(".main").width()-column*200-50)/2+(j)*230+'px';
-					}
-					       
-					boxlist[i].style.bottom="";
-					boxlist[i].style.left=left;
-					boxlist[i].style.top=listHeight[j];
-					listHeight[j]=parseInt(listHeight[j])+parseInt(getComputedStyle(boxlist[i],null)["height"])+30+'px';
-					//alert(listHeight);
+						putList(boxlist);//安序排放
+					} })
 				}
-			//alert();
-		} })
-	}
-}
-function SetMainHeight (height) {
-	if(!arguments.length)
-		var heights=$(window).height()*1.4+$(window).scrollTop();
-
-	else
-	{
-		var heights= height;
-	}
-	$('.main').height(heights);
-	//alert($(window).height());
-}
-	
-function StartAtLeft (classname) {
-	var boxlist=$(classname);
-	//for (box in boxlist){
-		boxlist.css('left','0');
-		boxlist.css('bottom','0');
-		boxlist.css('opacity','1');
-		boxlist.css('transition','3s');
-
-	//}
-	return boxlist;
-}
-
-
-function getColumn(){
-	var main = $('.main');
-	//alert(main.css('width'));
-	var columnNum = Math.floor((parseInt(main.css('width'))-2*parseInt(main.css('padding-left')))/200);
-	//alert(columnNum);
-	return columnNum;
-}
-
-function setList(){
-	column=getColumn();
-	listHeight=new Array(column);
-
-	for(var i = 0;i<listHeight.length;i++)
-	{
-		
-		if(!listHeight[i])
-		{
-			listHeight[i]=0+"px";
-		}
-	}
-	boxlist=StartAtLeft(".box");
-	var j=0;
-	for(var i =0;i<boxlist.length;i++)
-	{
-		
-		j=getMin(listHeight);
-		// /alert(j+"---"+listHeight);
-		if(j==0)//第一行
-			{
-				
-				var left = ($(".main").width()-column*200-50)/2+'px';
 			}
-		else 
-		{
-			    var left =($(".main").width()-column*200-50)/2+(j)*230+'px';
+			function SetMainHeight (height) {//改变页面高度
+				if(!arguments.length)
+				{
 
-		}
-		boxlist[i].style.bottom="";
-		boxlist[i].style.left=left;
-		boxlist[i].style.top=listHeight[j];
-		listHeight[j]=parseInt(listHeight[j])+parseInt(getComputedStyle(boxlist[i],null)["height"])+30+'px';
-		//alert(listHeight);
-	}
-	return listHeight;
-}
-function getMin(arry){
-	var aim=new Array(arry.length) ;
-	for(var i = 0;i<arry.length;i++)
-	{	
+					var heights=parseInt(listHeight[getMax(listHeight)])+500+"px";
+					
+				}
 
-		aim[i]=(parseInt(arry[i]));
+				else
+				{
+					var heights= height;
+				}
+				$('.main').height(heights);
+			}
+				
+			function StartAtLeft (classname) {//设置加载初始位置
+				var boxlist=$(classname);
+					boxlist.css('left','45%');
+					boxlist.css('bottom','-20%');
+					boxlist.css('opacity','1');
+					boxlist.css('transition','5s');
+				return boxlist;
+			}
 
-	}
-	// /alert(i);
-	//alert("qian"+aim);
-	function sortNumber(a,b)
-	{
-		return a - b;
-	}
-	aim.sort(sortNumber);
-	//alert("hou"+aim);
-	return arry.indexOf(aim[0]+"px");
-}
+
+			function getColumn(){//计算列数
+				var main = $('.main');
+				var columnNum = Math.floor((parseInt(main.css('width'))-2*parseInt(main.css('padding-left')))/200);
+				return columnNum;
+			}
+
+			function setList(){//精确排放
+				column=getColumn();
+				listHeight=new Array(column);
+
+				for(var i = 0;i<listHeight.length;i++)
+				{
+					
+					if(!listHeight[i])
+					{
+						listHeight[i]=0+"px";
+					}
+				}
+				boxlist=StartAtLeft(".box");
+				putList(boxlist);
+				return listHeight;
+			}
+			function getMin(arry){//获取最小值索引
+				var aim=new Array(arry.length) ;
+				for(var i = 0;i<arry.length;i++)
+				{	
+
+					aim[i]=(parseInt(arry[i]));
+
+				}
+				function sortNumber(a,b)
+				{
+					return a - b;
+				}
+				aim.sort(sortNumber);
+				return arry.indexOf(aim[0]+"px");
+			}
+			function getMax(arry){//获取最大值索引
+				var aim=new Array(arry.length) ;
+				for(var i = 0;i<arry.length;i++)
+				{	
+
+					aim[i]=(parseInt(arry[i]));
+
+				}
+				function sortNumber(a,b)
+				{
+					return a - b;
+				}
+				aim.sort(sortNumber);
+				return arry.indexOf(aim[arry.length-1]+"px");
+			}
+			function putList (boxlist) {//排放算法
+				var j=0;
+						for(var i =0;i<boxlist.length;i++)//
+							{
+								j=getMin(listHeight);
+								
+								if(j==0)//第一列
+									{
+										
+										var left = ($(".main").width()-column*200-50)/2+'px';
+									}
+								else 
+								{
+									  var left =($(".main").width()-column*200-50)/2+(j)*230+'px';
+								}
+								       
+								boxlist[i].style.bottom="";
+								boxlist[i].style.left=left;
+								boxlist[i].style.top=listHeight[j];
+								listHeight[j]=parseInt(listHeight[j])+parseInt(getComputedStyle(boxlist[i],null)["height"])+30+'px';
+							}
+				
+			}
